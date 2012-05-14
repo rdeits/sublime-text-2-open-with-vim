860609acabcdd044a9897a8a0b45ed868896969f
Robin Deits <robin.deits@gmail.com>
2012-05-14 16:13:51 -0400

    Added support for gvim on Linux and macvim on OSX

--- OpenWithVim.py
+++ OpenWithVim.py
@@ -1,19 +1,40 @@
 import sublime
 import sublime_plugin
 import subprocess
+import platform
 
 class OpenWithVimCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        terminal = "gnome-terminal"
-        option = "--command"
-        vim = "/usr/bin/vim"
-        path = None
-
+    def get_path(self):
         if self.window.active_view():
-            path = "\"" + self.window.active_view().file_name() + "\""
+            return self.window.active_view().file_name()
         else:
             sublime.error_message(__name__ + ": No file to open.")
-            return
+            return None
 
-        args = [terminal, option, vim + " " + path]
+    def run(self):
+        path = self.get_path()
+        if path is not None:
+            self.open_vim(path)
+
+    def open_vim(self, path):
+        local_os = platform.system()
+        if local_os == 'Linux':
+            terminal = "gnome-terminal"
+            option = "--command"
+            vim = "/usr/bin/vim"
+            args = [terminal, option, vim + ' "' + path + '"']
+        else:
+            sublime.error_message("Sorry, I don't know how to open terminal vim on this platform: %s. Try open_with_gvim" % local_os)
+            return
         subprocess.Popen(args)
+
+
+class OpenWithGvimCommand(OpenWithVimCommand):
+    def open_vim(self, path):
+        local_os = platform.system()
+        if local_os == 'Darwin':
+            args = ["mvim", path]
+        else:
+            args = ["gvim", path]
+        subprocess.Popen(args)
+
